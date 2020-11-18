f566ed13abee0b14a653d1743eae2344eccb71dc
luispcunha <luispplmcunha@gmail.com>
2020-11-18 09:45:04 +0000

    Use async/await in database queries

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,16 +7,15 @@ app.get('/', (req, res) => {
     res.send('Welcome to Cimba Intercompany.');
 });
 
-app.get('/users', (req, res) => {
-    db('users').select()
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            console.log('Failed to establish connection to database! Exiting...');
-            console.log(err);
-            process.exit(1);
-        });
+app.get('/users', async (req, res) => {
+    try {
+        const users = await db('users').select();
+        res.send(users);
+    } catch(err) {
+        console.log('Failed to establish connection to database! Exiting...');
+        console.log(err);
+        process.exit(1);
+    }
 });
 
 app.listen(process.env.PORT, () => {
