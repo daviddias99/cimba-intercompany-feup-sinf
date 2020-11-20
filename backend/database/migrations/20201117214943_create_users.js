f09a13ff0c226031da1d594f48dbb52170bd327f
Filipa Senra <filipasenra4@gmail.com>
2020-11-20 22:20:26 +0000

    Added Username, Password and Hash to User

--- backend/database/migrations/20201117214943_create_users.js
+++ backend/database/migrations/20201117214943_create_users.js
@@ -1,7 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments();
-    table.string('name').notNullable();
+    table.string('username').notNullable();
+    table.string('password').notNullable();
+    table.unique('username');
   });
 };
 
--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,8 +1,9 @@
+const bcrypt = require('bcrypt');
 const db = require('../../database/knex');
 
 exports.all_users = async (req, res) => {
   try {
-    const users = await db('users').select();
+    const users = await db('users').select(['id', 'username']);
     res.send(users);
   } catch (err) {
     console.log('Failed to establish connection to database! Exiting...');
@@ -13,12 +14,17 @@ exports.all_users = async (req, res) => {
 
 exports.new_user = async (req, res) => {
   try {
-    if (!req.body.name) {
-      res.status(400).json('Expected to have the Name Argument for Creating an User!').send();
+    if (!req.body.username) {
+      res.status(400).json('Expected to have the Username Argument for Creating an User!').send();
       return;
     }
 
-    const user = await db('users').insert([{ name: req.body.name }], ['id', 'name']);
+    if (!req.body.password) {
+      res.status(400).json('Expected to have the Password Argument for Creating an User!').send();
+      return;
+    }
+
+    const user = await db('users').insert([{ username: req.body.username }, { password: bcrypt.hashSync('req.body.password', 10) }], ['id', 'username']);
     res.send(user);
   } catch (err) {
     console.log('Failed to establish connection to database! Exiting...');
