cd3508884761ec9226feea74356d2237b685a8e2
Filipa Senra <filipasenra4@gmail.com>
2020-11-21 13:37:08 +0000

    Added one more object to company seed

--- backend/database/seeds/01_company.js
+++ backend/database/seeds/01_company.js
@@ -5,5 +5,8 @@ exports.seed = function (knex) {
       {
         company_key: 'company_key', app_id: 'app_id', app_secret: 'app_secret', tenant: 'tenant', organization: 'organization',
       },
+      {
+        company_key: 'company_key1', app_id: 'app_id1', app_secret: 'app_secret1', tenant: 'tenant1', organization: 'organization1',
+      },
     ]));
 };
