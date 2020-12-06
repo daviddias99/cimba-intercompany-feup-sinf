ff3fd9879a3f5d8670ab0ae907246bd1385bb3de
EduRibeiro00 <eribeiro306@gmail.com>
2020-12-06 13:38:54 +0000

    Fixed alignment of navbar icons

--- application/src/pages/Mapping/index.js
+++ application/src/pages/Mapping/index.js
@@ -6,15 +6,26 @@ import Tabs from 'components/common/Tabs';
 import Tab from 'components/common/Tab';
 import Table from 'components/common/Table';
 
+// TODO: depois ver para onde é que a funcao poderia ficar
+const newItemMapping = () => {
+  // TODO: abrir modal, receber input e fazer ação
+  console.log("new item mapping")
+}
+
+const newCompanyMapping = () => {
+  // TODO: abrir modal, receber input e fazer ação
+  console.log("new company mapping")
+}
+
 const Mapping = () => {
 
   return (
     <Layout title='Mapping'>
       <Tabs>
-        <Tab label="Items" btntext="New Item Mapping" btnfunc={() => console.log("new item mapping")}>
+        <Tab label="Items" btntext="New Item Mapping" btnfunc={newItemMapping}>
           <Table columns={itemTableColumns} urltofetch={itemTableURL} />
         </Tab>
-        <Tab label="Companies" btntext="New Company Mapping" btnfunc={() => console.log("new company mapping")}>
+        <Tab label="Companies" btntext="New Company Mapping" btnfunc={newCompanyMapping}>
           <Table columns={companyTableColumns} urltofetch={companyTableURL} />
         </Tab>
       </Tabs>
