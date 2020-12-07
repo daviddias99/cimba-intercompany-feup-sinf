e9d2d45d43174e309c6486d7836c6c925c535b3b
Filipa Senra <filipasenra4@gmail.com>
2020-12-07 17:11:18 +0000

    Adjust DataBase Seeds and Item Maps
    Added company Key to itemMaps
    Added Seed to orders in order to only trigger process for last order

--- backend/database/migrations/20201117214943_create_schema.js
+++ backend/database/migrations/20201117214943_create_schema.js
@@ -24,10 +24,12 @@ exports.up = function (knex) {
       table.increments();
       table.string('local_id').notNullable();
       table.string('item_id').notNullable();
+      table.string('company_key').notNullable();
+      table.foreign('company_key').references('companies.company_key').onUpdate('CASCADE').onDelete('CASCADE');
       table.integer('company_id').unsigned();
       table.foreign('company_id').references('companies.id').onUpdate('CASCADE').onDelete('CASCADE');
 
-      table.unique(['local_id', 'company_id'], 'items_map_unique');
+      table.unique(['local_id', 'company_key', 'company_id'], 'items_map_unique');
     })
     .createTable('company_maps', (table) => {
       table.increments();
--- backend/database/seeds/03_company_maps.js
+++ backend/database/seeds/03_company_maps.js
@@ -2,11 +2,12 @@ exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('company_maps').del()
     .then(() => knex('company_maps').insert([
-      { local_id: '0002', company_key: 'SINF_AM_1', company_id: 1 },
-      { local_id: '0003', company_key: 'SINF_AM_1', company_id: 1 },
-      { local_id: '0004', company_key: 'SINF_AM_1', company_id: 1 },
-      { local_id: '0002', company_key: 'SINF_AM_2', company_id: 2 },
-      { local_id: '0003', company_key: 'SINF_AM_2', company_id: 2 },
-      { local_id: '0004', company_key: 'SINF_AM_2', company_id: 2 },
+      { local_id: '0001', company_key: 'SINF_AM_2', company_id: 1 },
+      { local_id: '0002', company_key: 'SINF_AM_2', company_id: 1 },
+      { local_id: '0003', company_key: 'SINF_AM_2', company_id: 1 },
+      { local_id: '0004', company_key: 'SINF_AM_2', company_id: 1 },
+      { local_id: '0002', company_key: 'SINF_AM_1', company_id: 2 },
+      { local_id: '0003', company_key: 'SINF_AM_1', company_id: 2 },
+      { local_id: '0004', company_key: 'SINF_AM_1', company_id: 2 },
     ]));
 };
