9f5db7e6fd631d008ada88ae028a0f9521ec2e15
luispcunha <luispplmcunha@gmail.com>
2020-12-09 23:32:39 +0000

    Refactor process controller

--- backend/routes/process.js
+++ backend/routes/process.js
@@ -2,8 +2,22 @@ const router = require('express').Router();
 const authenticate = require('../middlewares/authenticate');
 const regexNum = require('../helper/regexNum');
 const processController = require('../controllers/processController');
+const { getProcess } = require('../database/methods/orderMethods');
+
+async function processMiddleware(req, res, next) {
+  const process = await getProcess(req.params.id);
+
+  if (!process) {
+    res.status(404).json({ status: 404 });
+    return;
+  }
+
+  req.process = process;
+  next();
+}
 
 router.use(authenticate);
+router.use(`/:id(${regexNum})`, processMiddleware);
 
 router.get(`/:id(${regexNum})/order`, processController.getOrder);
 
