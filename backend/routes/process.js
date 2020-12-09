2dcc7918341335c02e7d1ac98d4f8f1791759f78
luispcunha <luispplmcunha@gmail.com>
2020-12-09 23:23:11 +0000

    Fix query that gets order from db

--- backend/routes/process.js
+++ backend/routes/process.js
@@ -2,23 +2,8 @@ const router = require('express').Router();
 const authenticate = require('../middlewares/authenticate');
 const regexNum = require('../helper/regexNum');
 const processController = require('../controllers/processController');
-const { getProcess } = require('../database/methods/orderMethods');
-
-
-async function processMiddleware(req, res, next) {
-  const process = await getProcess(req.params.id);
-
-  if (!process) {
-    res.status(404).json({ status: 404 });
-    return;
-  }
-
-  req.process = process;
-  next();
-}
 
 router.use(authenticate);
-router.use(processMiddleware);
 
 router.get(`/:id(${regexNum})/order`, processController.getOrder);
 
