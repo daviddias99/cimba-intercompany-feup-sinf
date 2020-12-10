df2532d734543d921089a8394482f1a08e93319d
Filipa Senra <filipasenra4@gmail.com>
2020-12-10 22:28:54 +0000

    Added create Invoice on Buyer

---
+++ backend/jasmin/invoices.js
@@ -0,0 +1,94 @@
+const { makeRequest } = require('./makeRequest');
+const { jasminToIcId, icToJasminId } = require('../database/methods/companyMapsMethods');
+const { getCompanyById } = require('../database/methods/companyMethods');
+const { getMapOfDocSalesOrder } = require('../database/methods/orderMapsMethods');
+const { addInvoiceToPurchaseOrder } = require('../database/methods/orderMethods');
+const { getOrderById } = require('./orders');
+
+async function getAvailableLinesForInvoice(buyer) {
+  return (await makeRequest(
+    'invoiceReceipt/processOrders/1/50',
+    'get',
+    buyer.id,
+    { company: buyer.company_key },
+    {},
+  )).data;
+}
+
+async function getOrdersKeyAndLines(orderIds, lines, icIdBuyer) {
+  // Getting Keys and lines For All orders
+  const orderKeysAndLines = [];
+  for (let i = 0; i < orderIds.length && i < lines.length; i += 1) {
+    const docId = orderIds[i];
+
+    if (docId == null) throw new ReferenceError(`Cannot find Sales Order to Purchase Order at Index ${i}`);
+
+    // eslint-disable-next-line no-await-in-loop
+    const orderBuyer = await getOrderById(icIdBuyer, docId);
+
+    orderKeysAndLines.push({
+      key: `${orderBuyer.data.documentType}.${orderBuyer.data.serie}.${orderBuyer.data.seriesNumber}`,
+      line: lines[i],
+    });
+  }
+
+  return orderKeysAndLines;
+}
+
+exports.createInvoice = async (
+  jasminIdBuyer, // buyerCustomerParty
+  icIdSuplier,
+  documentLines,
+) => {
+  // Getting intercompany id of buyer
+  const icIdBuyer = await jasminToIcId(icIdSuplier, jasminIdBuyer);
+  if (icIdBuyer == null) throw new ReferenceError(`Cannot Map Buyer ${jasminIdBuyer} in Suplier`);
+
+  // Getting local id of suplier in buyer
+  const jasminIdSuplier = await icToJasminId(icIdBuyer, icIdSuplier);
+  if (jasminIdSuplier == null) throw new ReferenceError(`Cannot Map Suplier ${icIdSuplier} in Buyer`);
+
+  const buyer = await getCompanyById(icIdBuyer);
+  if (buyer == null) throw new ReferenceError(`Cannot Find Suplier with id ${icIdBuyer}`);
+
+  // Translate SalesOrderIds to PurchaseOrderIds
+  let purchaseOrderIds = [];
+  const lines = [];
+  documentLines.forEach((element) => {
+    purchaseOrderIds.push(getMapOfDocSalesOrder(element.sourceDocId, icIdSuplier));
+    lines.push(element.sourceDocLine);
+  });
+
+  purchaseOrderIds = await Promise.all(purchaseOrderIds);
+
+  // Getting Keys and Lines For all Entries
+  const orderKeysAndLines = await getOrdersKeyAndLines(purchaseOrderIds, lines, icIdBuyer);
+
+  // Filter the available lines with the lines sent by the suplier
+  const availableLinesForInvoice = await getAvailableLinesForInvoice(buyer);
+
+  availableLinesForInvoice.filter((element) => orderKeysAndLines.includes(
+    { key: element.orderKey, line: element.orderLineNumber },
+  ));
+
+  const invoices = await makeRequest(
+    `invoiceReceipt/processOrders/${buyer.company_key}`,
+    'post',
+    buyer.id,
+    {},
+    availableLinesForInvoice,
+  );
+
+  if (invoices.status !== 201) {
+    return invoices;
+  }
+
+  const buyerOrderIds = new Set(purchaseOrderIds);
+  buyerOrderIds.forEach(
+    (sourceDocId) => addInvoiceToPurchaseOrder(icIdBuyer, sourceDocId, invoices.data),
+  );
+
+  console.log(`Created Invoice ${invoices.data} for company ${icIdBuyer}`);
+
+  return invoices;
+};
