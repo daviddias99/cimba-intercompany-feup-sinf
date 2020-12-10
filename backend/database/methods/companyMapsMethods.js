e19365cca523bb06198da64c5bff0e3ecdf0e500
Filipa Senra <filipasenra4@gmail.com>
2020-12-10 15:05:21 +0000

    Changed companyId to icId in companyMaps

--- backend/database/methods/companyMapsMethods.js
+++ backend/database/methods/companyMapsMethods.js
@@ -1,12 +1,12 @@
 const db = require('../knex');
 
-exports.jasminToIcId = async (companyId, jasminId) => {
-  const company = await db('company_maps').where({ jasmin_id: jasminId, ic_id: companyId }).first();
+exports.jasminToIcId = async (icId, jasminId) => {
+  const company = await db('company_maps').where({ jasmin_id: jasminId, ic_id: icId }).first();
   return company == null ? null : company.map_ic_id;
 };
 
-exports.icToJasminId = async (companyId, mapCompanyId) => {
-  const company = await db('company_maps').where({ map_ic_id: mapCompanyId, ic_id: companyId }).first();
+exports.icToJasminId = async (icId, mapicId) => {
+  const company = await db('company_maps').where({ map_ic_id: mapicId, ic_id: icId }).first();
 
   return company == null ? null : company.jasmin_id;
 };
