e276d9d30ab8c6ff0c7eb6dbfd9c88223ce536b7
daviddias99 <up201705373@fe.up.pt>
2020-12-15 00:15:12 +0000

    Corrected db seed

--- backend/database/seeds/04_item_maps.js
+++ backend/database/seeds/04_item_maps.js
@@ -8,5 +8,11 @@ exports.seed = function (knex) {
       {
         jasmin_id: '2222', item_id: '1111', map_ic_id: 1, ic_id: 2, item_quant: 0.5,
       },
+      {
+        jasmin_id: '1234', item_id: '0001', map_ic_id: 2, ic_id: 1, item_quant: 5,
+      },
+      {
+        jasmin_id: '0001', item_id: '1234', map_ic_id: 1, ic_id: 2, item_quant: 0.2,
+      },
     ]));
 };
